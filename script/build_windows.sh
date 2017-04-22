db1f5c3a8447ec06116135bf6c50777b4418d7c7
hernad <hernad@bring.out.ba>
2017-04-22 13:42:06 +0200

    build in container

--- script/build_windows.sh
+++ script/build_windows.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
-export USE_CONTAINER=true
+#export USE_CONTAINER=true
 
-GO=/usr/local/go/bin/go
-GOROOT=/usr/local/go
-GOPATH="/go"
-
-#TARGET_OS=windows TARGET_ARCH="amd64 386" make build-x
+TARGET_OS=windows TARGET_ARCH="amd64 386" ./script/build_in_container.sh clean build-x
 
 
-FLAGS="-a  -tags \"static_build netgo \" -installsuffix netgo -ldflags \"-X `go list ./version`.GitCommit=`git rev-parse --short HEAD 2>/dev/null` -w -s -extldflags -static\"  ./cmd/machine.go"
+#FLAGS="-a  -tags \"static_build netgo \" -installsuffix netgo -ldflags \"-X `go list ./version`.GitCommit=`git rev-parse --short HEAD 2>/dev/null` -w -s -extldflags -static\"  ./cmd/machine.go"
 
-$GO get -d github.com/docker/machine github.com/codegangsta/cli
+#$GO get -d github.com/docker/machine github.com/codegangsta/cli
 
-GOOS=windows GOARCH=amd64 CGO_ENABLED=0 \
-   $GO build -o /go/src/github.com/hernad/docker-machine/bin/docker-machine-Windows-x86_64.exe \
-   $FLAGS
+#GOOS=windows GOARCH=amd64 CGO_ENABLED=0 \
+#   $GO build -o /go/src/github.com/hernad/docker-machine/bin/docker-machine-Windows-x86_64.exe \
+#   $FLAGS
 
-GOOS=windows GOARCH=386 CGO_ENABLED=0 \
-   $GO build -o /go/src/github.com/hernad/docker-machine/bin/docker-machine-Windows-i386.exe \
-   $FLAGS
+#GOOS=windows GOARCH=386 CGO_ENABLED=0 \
+#   $GO build -o /go/src/github.com/hernad/docker-machine/bin/docker-machine-Windows-i386.exe \
+#   $FLAGS
 
